436042d4998108af5f20bd8b2418454bfddf6e06
Sergey <108259528+Maclovi@users.noreply.github.com>
2023-05-21 11:34:01 +0300

    Update getmusic.py

--- getmusic.py
+++ getmusic.py
@@ -3,35 +3,39 @@ import io
 
 
 class LimitSizeError(Exception):
+
     def __init__(self, message: str = "Размер файла превышает 50мб.") -> None:
-		    super().__init__(message)
+        super().__init__(message)
 
 
 class YouTubeMusic:
-  
-    def __init__(self, url: str = "https://youtu.be/GCdwKhTtNNw"):
-    	  # Экземпляр объекта YouTube
-    	  self.yt: object = YouTube(url)
 
-    	  self.views: int = self.yt.views  # Просмотры
-    	  self.pub: int = self.yt.publish_date.year  # Год публикации
-    	  self.thumb: str = self.yt.thumbnail_url  # Заглавная картинка
-    	  self.title: str = self.yt.title  # Название песни
+    def __init__(self, url: str):
 
-    	  self.audio = self.yt.streams.get_audio_only()  # Берёт itag 140
+    # Экземпляр объекта YouTube
+    self.yt: object = YouTube(url)
 
-    def check(self) -> bool | str:
-  	    try:
-  		      self.yt.check_availability()
-            if self.audio.filesize_mb > 50:
-        	      raise LimitSizeError
-      	return True
+    self.views: int = self.yt.views  # Просмотры
+    self.pub: int = self.yt.publish_date.year  # Год публикации
+    self.thumb: str = self.yt.thumbnail_url  # Заглавная картинка
+    self.title: str = self.yt.title  # Название песни
+
+    self.audio = self.yt.streams.get_audio_only()  # Берёт itag 140
+
+
+def check(self) -> bool | object:
+    try:
+        self.yt.check_availability()
+        if self.audio.filesize_mb > 50:
+            raise LimitSizeError
+        return True
 
     except Exception as error:
         return error
 
-    def get_music(self):
-        with io.BytesIO() as output:
-            self.audio.stream_to_buffer(output)
 
-            return output.getvalue()
+def get_music(self):
+    with io.BytesIO() as output:
+        self.audio.stream_to_buffer(output)
+
+    return output.getvalue()
