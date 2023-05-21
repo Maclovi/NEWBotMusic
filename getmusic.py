e55a78773cd6875275da2981867a32e49ca2bdaf
Sergey <108259528+Maclovi@users.noreply.github.com>
2023-05-21 11:48:37 +0300

    Update getmusic.py

--- getmusic.py
+++ getmusic.py
@@ -13,7 +13,7 @@ class YouTubeMusic:
     def __init__(self, url: str):
 
         # Экземпляр объекта YouTube
-        self.yt: object = YouTube(url)
+        self.yt = YouTube(url)
 
         self.views: int = self.yt.views  # Просмотры
         self.pub: int = self.yt.publish_date.year  # Год публикации
@@ -23,19 +23,19 @@ class YouTubeMusic:
         self.audio = self.yt.streams.get_audio_only()  # Берёт itag 140
 
 
-def check(self) -> bool | object:
-    try:
-        self.yt.check_availability()
-        if self.audio.filesize_mb > 50:
-            raise LimitSizeError
-        return True
-
-    except Exception as error:
-        return error
-
-
-def get_music(self):
-    with io.BytesIO() as output:
-        self.audio.stream_to_buffer(output)
-
-    return output.getvalue()
+    def check(self) -> bool | object:
+        try:
+            self.yt.check_availability()
+            if self.audio.filesize_mb > 50:
+                raise LimitSizeError
+            return True
+    
+        except Exception as error:
+            return error
+    
+    
+    def get_music(self):
+        with io.BytesIO() as output:
+            self.audio.stream_to_buffer(output)
+    
+            return output.getvalue()
