f74d551daea65ae1fceb0f4c94d84a97af377fba
Sergey Yavorsky <yavorskyserge@gmail.com>
2023-05-23 15:32:52 +0300

    function optimized and main

--- getmusic.py
+++ getmusic.py
@@ -11,31 +11,25 @@ class LimitSizeError(Exception):
 class YouTubeMusic:
 
     def __init__(self, url: str):
-
-        # Экземпляр объекта YouTube
-        self.yt = YouTube(url)
-
-        self.views: int = self.yt.views  # Просмотры
-        self.pub: int = self.yt.publish_date.year  # Год публикации
-        self.thumb: str = self.yt.thumbnail_url  # Заглавная картинка
-        self.title: str = self.yt.title  # Название песни
-
-        self.audio = self.yt.streams.get_audio_only()  # Берёт itag 140
-
-
-    def check(self) -> bool | object:
         try:
-            self.yt.check_availability()
+            self.yt: object = YouTube(url)  # Экземпляр объекта YouTube 
+            self.audio = self.yt.streams.get_audio_only()
             if self.audio.filesize_mb > 50:
                 raise LimitSizeError
-            return True
-    
+            self.response_valid = True
         except Exception as error:
-            return error
-    
+            self.response_valid = error    
+        else:
+            self.views: int = self.yt.views  # Просмотры
+            self.pub: int = self.yt.publish_date.year   # Год публикации
+            self.thumb: str = self.yt.thumbnail_url  # Заглавная картинка
+            self.title: str = self.yt.title  # Название песни
+
+            del self.yt
+
     
     def get_music(self):
         with io.BytesIO() as output:
             self.audio.stream_to_buffer(output)
-    
+
             return output.getvalue()
